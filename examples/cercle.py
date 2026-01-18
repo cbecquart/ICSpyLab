597ae75725bc8815283ad88417cb3d39fe8209f2
cbecquart <146818693+cbecquart@users.noreply.github.com>
2026-01-18 14:34:26 +0100

    Update cercle.py

--- examples/cercle.py
+++ examples/cercle.py
@@ -1,25 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from icspylab import ICS, cov, tcov
 
-def generate_points_on_random_ball(n, p, radius=1):
-    """
-    Generate random points in d dimensions that have uniform probability over the unit ball scaled by "radius" (length
-    of points are in range [0, "radius"]).
-    Source: https://stackoverflow.com/questions/54544971/how-to-generate-uniform-random-points-inside-d-dimension-ball-sphere
-    :param n: (int) number of points to generate
-    :param p: (int) dimension
-    :param radius: (float) radius of the d-ball
-    :return: (n, d) array with the generated points
-    """
-    # Distribute evenly on ball by normalizing the length of a vector of random-normal values
-    random_directions = np.random.normal(size=(p, n))
-    random_directions /= np.linalg.norm(random_directions, axis=0)
-    # Generate a random radius with probability proportional to the surface area of a ball with a given radius
-    # random_radii = np.random.random(n) ** (1 / p)
-    # Return the list of random (direction & length) points.
-    return radius * (random_directions).T
-
-def points_uniformes_sphere(n, p, rayon=np.sqrt(2)):
+def points_uniform_circle(n, p=2, rayon=np.sqrt(2)):
     x = np.random.normal(size=(n, p))
     norms = np.linalg.norm(x, axis=1, keepdims=True)
     return rayon * x / norms
@@ -31,7 +15,7 @@ n = 500
 
 rng = np.random.default_rng(seed=0)
 
-signal = points_uniformes_sphere(n, 2, rayon=np.sqrt(2))
+signal = points_uniform_circle(n, p=2, rayon=np.sqrt(2))
 
 X1 = signal + rng.multivariate_normal(
     mean=np.zeros(2),
@@ -48,4 +32,19 @@ X2 = rng.multivariate_normal(
 X = np.hstack((X1, X2))
 
 plt.scatter(X[:, 0], X[:, 1])
+plt.show()
+
+# PCA
+
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
+
+plt.scatter(X_pca[:, 0], X_pca[:, 1])
+plt.show()
+
+# ICS
+
+ics = ICS(S1=tcov, S2=cov, algorithm='standard')
+ics.fit_transform(X)
+plt.scatter(ics.scores_[:, 0], ics.scores_[:, 1])
 plt.show()
